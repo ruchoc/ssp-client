faf62a637cbb90e25695d45010b1afbac51dd71c
ruchoc <ruchoc@qq.com>
2022-05-21 09:47:13 +0800

    修改同源策略

--- src/global/config.js
+++ src/global/config.js
@@ -1,7 +1,7 @@
 const pageSize = 10;
 const listLength = 3;
 
-const proxyPath = "/api";
-// const proxyPath = "";
+// const proxyPath = "/api";
+const proxyPath = "";
 
 export { pageSize, listLength, proxyPath };
